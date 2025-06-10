df3ff7e9acd4225a3af9a07d72dfa9cbcb0fbf6a
Trumin <tonyhoooooo@outlook.com>
2025-06-10 22:33:41 +0800

    添加缩放功能

--- client/image_window/image_view.py
+++ client/image_window/image_view.py
@@ -1,35 +1,68 @@
-from PyQt6.QtWidgets import QWidget, QLabel, QVBoxLayout
-from PyQt6.QtGui import QPixmap
-from PyQt6.QtCore import Qt
+from PyQt6.QtWidgets import QGraphicsView, QGraphicsScene, QGraphicsPixmapItem, QLabel, QVBoxLayout
+from PyQt6.QtGui import QPixmap, QWheelEvent, QMouseEvent
+from PyQt6.QtCore import Qt, QRectF
 
 
-class ImageView(QWidget):
+class ImageView(QGraphicsView):
     def __init__(self):
         super().__init__()
-        self.image_label = QLabel("图像未加载")
-        self.image_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+        self.setScene(QGraphicsScene(self))
+        self.pixmap_item = QGraphicsPixmapItem()
+        self.scene().addItem(self.pixmap_item)
 
-        layout = QVBoxLayout()
-        layout.addWidget(self.image_label)
-        self.setLayout(layout)
+        self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarPolicy.ScrollBarAsNeeded)
+        self.setTransformationAnchor(QGraphicsView.ViewportAnchor.AnchorUnderMouse)
+        
+        self.scale_factor = 1.15 # 每次的缩放比例
+        self._zoom = 0 # 当前缩放等级
 
         self.controller = None
+        self._keep_zoom = False
 
     def set_controller(self, controller):
         self.controller = controller
     
     def display_image(self, pixmap: QPixmap):
-        if pixmap.isNull():
-            self.image_label.setText("图像为空")
-        else:
-            self.image_label.setPixmap(
-                pixmap.scaled(
-                    self.image_label.size(),
-                    Qt.AspectRatioMode.KeepAspectRatio,
-                    Qt.TransformationMode.SmoothTransformation    
-                )
-            )
+        self.pixmap_item.setPixmap(pixmap)
+        self.setSceneRect(QRectF(pixmap.rect()))
+        if not self._keep_zoom:
+            self._zoom = 0
+            self.resetTransform()
+            self.fitInView(self.pixmap_item, Qt.AspectRatioMode.KeepAspectRatio)
+            self._keep_zoom = True
+        
+
     
+    def wheelEvent(self, event: QWheelEvent):
+        if event.angleDelta().y() > 0:
+            factor = self.scale_factor
+            self._zoom += 1
+        else:
+            factor = 1 / self.scale_factor
+            self._zoom -= 1
+
+        # 限制缩放范围
+        if self._zoom < -10:
+            self._zoom = -10
+            return
+        elif self._zoom > 20:
+            self._zoom = 20
+            return
+
+        self.scale(factor, factor)
+
+    def mousePressEvent(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event: QMouseEvent):
+        if event.button() == Qt.MouseButton.LeftButton:
+            self.setDragMode(QGraphicsView.DragMode.ScrollHandDrag)
+        super().mouseReleaseEvent(event)
+
     def resizeEvent(self, event):
         super().resizeEvent(event)
         if self.controller:
